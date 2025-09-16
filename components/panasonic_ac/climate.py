8daaa2d0bd802d8a39371cfe14035446609e77f9
cho0p <42189739+cho0p@users.noreply.github.com>
2025-09-16 16:04:55 +0200

    Update climate.py

--- components/panasonic_ac/climate.py
+++ components/panasonic_ac/climate.py
@@ -74,7 +74,7 @@ SCHEMA = climate.CLIMATE_SCHEMA.extend(
             state_class=STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional(CONF_NANOEX_SWITCH): SWITCH_SCHEMA,
-        cv.Optional(CONF_OUTSIDE_TEMPERATURE_OFFSET): cv.int_range(min=-5, max=5),
+        cv.Optional(CONF_OUTSIDE_TEMPERATURE_OFFSET): cv.int_range(min=-9, max=9),
     }
 ).extend(uart.UART_DEVICE_SCHEMA)
 
@@ -92,7 +92,7 @@ CONFIG_SCHEMA = cv.typed_schema(
                 cv.Optional(CONF_ECONAVI_SWITCH): SWITCH_SCHEMA,
                 cv.Optional(CONF_MILD_DRY_SWITCH): SWITCH_SCHEMA,
                 cv.Optional(CONF_CURRENT_TEMPERATURE_SENSOR): cv.use_id(sensor.Sensor),
-                cv.Optional(CONF_CURRENT_TEMPERATURE_OFFSET): cv.int_range(min=-5, max=5),
+                cv.Optional(CONF_CURRENT_TEMPERATURE_OFFSET): cv.int_range(min=-9, max=9),
                 cv.Optional(CONF_CURRENT_POWER_CONSUMPTION): sensor.sensor_schema(
                   unit_of_measurement=UNIT_WATT,
                   accuracy_decimals=0,
